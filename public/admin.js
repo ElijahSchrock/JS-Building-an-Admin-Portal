9c9ea6951200d3f9f87cd9371fe5e2830d743d3a
Elijah Schrock <70502486+ElijahSchrock@users.noreply.github.com>
2021-12-06 16:32:14 -0800

    project done, cant get page updated

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,64 @@
 
-// Your Code Here
+async function main() {
+
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+    console.log(books)
+}
+function renderBook(book) {
+    let root = document.querySelector('#root');
+
+    let li = document.createElement('li');
+    li.textContent = book.title
+        root.append(li)
+    
+    let numOfBooks = document.createElement('input');
+    numOfBooks.value = book.quantity
+        root.append(numOfBooks);
+
+    let saveBttn = document.createElement('button')
+        saveBttn.textContent = 'Save'
+    
+        saveBttn.addEventListener('click', function () {
+            response = fetch('http://localhost:3001/updateBook'), {
+                method : 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    id: book.id,
+                    quantity: numOfBooks.value,
+                }),
+            }
+            return response.json();
+        })
+            root.append(saveBttn);
+    
+    let deleteBttn = document.createElement('button');
+        deleteBttn.textContent = 'Delete'
+
+        deleteBttn.addEventListener('click', () => {
+            fetch('http://localhost:3001/updateBook'), {
+                    method: 'DELETE',
+        }
+    })
+        root.append(deleteBttn);
+}
+main()
+
+// async function update(){
+//         respone = await fetch('http://localhost:3001/updateBook'), {
+//         method : 'PATCH',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//             body: JSON.stringify({
+//             id: book.id,
+//             quantity: numOfBooks.value,
+//         }),
+//     }
+//     let updatedBook = await response.json();
+//     }
+    
